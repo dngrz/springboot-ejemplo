91fb3bd70d3f8cddd4faf519029c246912ee51f5
Clodoaldo Sánchez <clodoaldosanchez@gmail.com>
2021-10-19 23:00:30 -0500

    feat: spring-cloud-config-server

---
+++ cloud-config-server/src/test/java/pe/net/csweb/microservicios/cloudconfigserver/CloudConfigServerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.net.csweb.microservicios.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
